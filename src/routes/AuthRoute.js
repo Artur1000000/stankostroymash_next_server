fa8fdd8ff5da639785210de625a25706839ee754
Artur1000000 <aarutyunov852005@gmail.com>
2023-05-11 07:53:02 +0300

    addd

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -3,6 +3,7 @@ import { Auth } from "../controllers/Auth.js";
 import { firstLogin } from "../utils/FirstLogin.js";
 
 const AuthRoute = new Router();
+
 AuthRoute.post("/auth", firstLogin, Auth);
 
 export { AuthRoute };
